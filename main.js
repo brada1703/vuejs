45b0d138e0dc6a99132a2388e355db56087dd9cf
Brad <bradley.ahrens@kwanko.com>
2018-07-08 22:32:12 +0100

    v-on:click

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello world',
-        link: 'http://google.com',
-        finishedLink: '<a href="http://google.com">Google</a>',
-        name: 'Brad',
-        age: 32,
+        counter: 0
     },
     methods: {
-        sayHello: function() {
-            this.title = 'Hello!';
-            return this.title;
-        },
-        random: function () {
-            return Math.random();
+        increase: function () {
+            this.counter++;
         }
     }
 })
